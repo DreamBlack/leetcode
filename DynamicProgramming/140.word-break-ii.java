944a8493e9e9a347d7230920032d0a4b90100a8c
Dream <1104122155@qq.com>
2019-12-21 10:24:00 +0800

    140.word-break-ii返回数组的dfs

---
+++ DynamicProgramming/140.word-break-ii.java
@@ -0,0 +1,102 @@
+import java.util.List;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode id=140 lang=java
+ *
+ * [140] Word Break II
+ *
+ * https://leetcode.com/problems/word-break-ii/description/
+ *
+ * algorithms
+ * Hard (28.16%)
+ * Likes:    1330
+ * Dislikes: 295
+ * Total Accepted:    188.2K
+ * Total Submissions: 649.1K
+ * Testcase Example:  '"catsanddog"\n["cat","cats","and","sand","dog"]'
+ *
+ * Given a non-empty string s and a dictionary wordDict containing a list of
+ * non-empty words, add spaces in s to construct a sentence where each word is
+ * a valid dictionary word. Return all such possible sentences.
+ * 
+ * Note:
+ * 
+ * 
+ * The same word in the dictionary may be reused multiple times in the
+ * segmentation.
+ * You may assume the dictionary does not contain duplicate words.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:
+ * s = "catsanddog"
+ * wordDict = ["cat", "cats", "and", "sand", "dog"]
+ * Output:
+ * [
+ * "cats and dog",
+ * "cat sand dog"
+ * ]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input:
+ * s = "pineapplepenapple"
+ * wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
+ * Output:
+ * [
+ * "pine apple pen apple",
+ * "pineapple pen apple",
+ * "pine applepen apple"
+ * ]
+ * Explanation: Note that you are allowed to reuse a dictionary word.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input:
+ * s = "catsandog"
+ * wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * Output:
+ * []
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    Map<String,LinkedList<String>>map;
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        map=new HashMap<String,LinkedList<String>>();
+        return dfs(s,wordDict);
+    }
+    List<String> dfs(String s,List<String>words){
+        LinkedList<String> res=new LinkedList<>();
+        //退出语句
+        if(s.length()==0){
+            res.add("");//这里必须加一个元素再返回，否则for(String t:tmp){这里根本不会执行，因为是empty(),但不是Null
+            return res;
+        }
+        if(map.containsKey(s)){
+            return map.get(s);
+        }
+        for(int j=0;j<s.length();j++){
+            if(words.contains(s.substring(0, j+1))){
+                List<String>tmp=dfs(s.substring(j+1),words);
+                for(String t:tmp){
+                    res.add(s.substring(0, j+1) + (t==""?"":" ")+t);
+                }
+               
+            }
+        }
+        map.put(s, res);
+        return res;
+       
+    }
+}
+// @lc code=end
+
